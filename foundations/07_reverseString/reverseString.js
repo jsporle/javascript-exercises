fd3c976fe8842b4c2f9cd693a1472e612131af68
jsporle <89423014+jsporle@users.noreply.github.com>
2026-02-04 09:36:33 +0000

    run all tests

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let stringReversed = "";
+    let i = string.length - 1;
+    while (i >= 0) {
+        stringReversed += string[i]
+        i--
+    }
+    return stringReversed;
 };
+
+console.log(reverseString("hello!"))
 
 // Do not edit below this line
 module.exports = reverseString;
