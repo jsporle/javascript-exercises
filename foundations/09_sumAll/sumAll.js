328364e641684e1c2becc06f3e2bb37a730b2a4b
jsporle <89423014+jsporle@users.noreply.github.com>
2026-02-04 10:36:58 +0000

    pass all tests

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,6 +1,31 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if (a === b) {
+        return a + b;
+    }
+    if ((a < 0) || (b < 0) ||
+        (typeof a !== "number") || (typeof b !== "number") || 
+        (!Number.isInteger(a)) || (!Number.isInteger(b))) { 
+        return "ERROR";
+    } 
+    if (a > b) {
+        let temp = a
+        a = b
+        b = temp
+    }
 
+    let sum = 0
+    while (a <= b) {
+        sum += a;
+        a++;
+    }
+
+    return sum
 };
+
+console.log(sumAll(1,3))
+
+
+
 
 // Do not edit below this line
 module.exports = sumAll;
