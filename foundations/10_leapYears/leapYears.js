791247534307c19b5f4e85fd6cad459ea95a1079
jsporle <89423014+jsporle@users.noreply.github.com>
2026-02-04 10:47:46 +0000

    process leap year

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,6 +1,23 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if ((year % 4 === 0)) {
+        if (year % 100 === 0) {
+            if (year % 400 !== 0) {
+                return false;
+            }
+        }
+        return true;
+    } 
+    return false;
 };
+
+console.log(leapYears(2000));
+console.log(leapYears(1985));
+
+//leap years are every four years
+//if years divisible by 100 they are not leap years unless
+    //they are divisible by 400
+//year must be greater or equal to zero
+
 
 // Do not edit below this line
 module.exports = leapYears;
