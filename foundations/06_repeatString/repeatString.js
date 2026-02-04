11d7756804dc79fb3e06b4fbaa86925a567b64d7
jsporle <89423014+jsporle@users.noreply.github.com>
2026-02-04 08:54:03 +0000

    repeat string

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let i = num;
+    let result = ""
+    while (i > 0) {
+    result += string;
+    i--}
+    return result;
 };
+
+console.log(repeatString('hey', 3))
 
 // Do not edit below this line
 module.exports = repeatString;
