1943ac6948ebec477202e6e557b2bc1ff5a12bb8
jsporle <89423014+jsporle@users.noreply.github.com>
2026-02-04 10:03:48 +0000

    pass all tests

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,8 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...itemtoRemove) {
+    return array.filter(item => !itemtoRemove.includes(item))
 };
+
+console.log(removeFromArray([1, 2, 3, 4], 2))
 
 // Do not edit below this line
 module.exports = removeFromArray;
