a2da420661e75e72856732b18f36cc974cff2c3c
jsporle <89423014+jsporle@users.noreply.github.com>
2026-02-04 08:55:51 +0000

    return ERROR for 0 or below

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,12 +1,18 @@
 const repeatString = function(string, num) {
-    let i = num;
+    if (num > 0) {
+        let i = num;
     let result = ""
     while (i > 0) {
     result += string;
     i--}
     return result;
+    } else {
+        return "ERROR"
+    }
+    
 };
 
+console.log(repeatString('hey', -1))
 console.log(repeatString('hey', 3))
 
 // Do not edit below this line
