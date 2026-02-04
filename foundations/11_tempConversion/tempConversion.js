9ca8fe23120579d8627e45f7be267f70dd5f6883
jsporle <89423014+jsporle@users.noreply.github.com>
2026-02-04 11:17:02 +0000

    return num instead of string

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,19 +1,13 @@
 const convertToCelsius = function(inputFahrenheit) {
   // c = (f − 32) × ⁠5/9
-  let outputCelsius = ((inputFahrenheit - 32) * (5 / 9));
-  if (!Number.isInteger(outputCelsius)) {
-    return outputCelsius.toFixed(1);
-  }
-  return outputCelsius;  
+  let outputCelsius = (inputFahrenheit - 32) * (5 / 9);
+  return +outputCelsius.toFixed(1)
 };
 
 const convertToFahrenheit = function(inputCelsius) {
   // f = (⁠9/5c + 32)
-  let outputFahrenheit = ((inputCelsius * (9 / 5)) + 32);
-  if (!Number.isInteger(outputFahrenheit)) {
-    return outputFahrenheit.toFixed(1);
-  }
-  return outputFahrenheit; 
+  let outputFahrenheit = (inputCelsius * (9 / 5)) + 32;
+  return +outputFahrenheit.toFixed(1); 
 };
 
 console.log(convertToCelsius(-10))
