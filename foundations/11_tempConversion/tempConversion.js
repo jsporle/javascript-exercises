8b5b4ff714b321a940631adc67327c2844265072
jsporle <89423014+jsporle@users.noreply.github.com>
2026-02-04 11:09:49 +0000

    leave as integer unless decimals follow

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,8 +1,23 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(inputFahrenheit) {
+  // c = (f − 32) × ⁠5/9
+  let outputCelsius = ((inputFahrenheit - 32) * (5 / 9));
+  if (!Number.isInteger(outputCelsius)) {
+    return outputCelsius.toFixed(1);
+  }
+  return outputCelsius;  
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(inputCelsius) {
+  // f = (⁠9/5c + 32)
+  let outputFahrenheit = ((inputCelsius * (9 / 5)) + 32);
+  if (!Number.isInteger(outputFahrenheit)) {
+    return outputFahrenheit.toFixed(1);
+  }
+  return outputFahrenheit; 
 };
+
+console.log(convertToCelsius(-10))
+console.log(convertToFahrenheit(-10))
 
 // Do not edit below this line
 module.exports = {
