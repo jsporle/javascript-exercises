75d92e417bbd61e0dd30dd54bfc06e3555f22bdd
jsporle <89423014+jsporle@users.noreply.github.com>
2026-02-16 19:04:38 +0000

    complete

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,6 +1,23 @@
-const palindromes = function () {
+const palindromes = function (input) {
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_expressions
+    const str = input.toLowerCase().replace(/[^a-z0-9]/g, "");
 
+    let fromStart = 0;
+    let fromEnd = str.length -1;
+    
+    while (fromStart < fromEnd) {
+        if (str[fromStart] !== str[fromEnd]) {
+            return false;
+        }
+    fromStart++;
+    fromEnd--;
+    }
+    
+    return true;
 };
+
+console.log(palindromes("hello!"))
+console.log(palindromes("hellolleh"))
 
 // Do not edit below this line
 module.exports = palindromes;
