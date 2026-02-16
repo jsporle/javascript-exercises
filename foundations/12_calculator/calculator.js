0e92e46d3cabfd1111d9a43d80ff14f0fd13253d
jsporle <89423014+jsporle@users.noreply.github.com>
2026-02-16 18:43:33 +0000

    complete

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -6,21 +6,36 @@ const subtract = function(a, b) {
   return(a - b);
 };
 
-const sum = function() {
+const sum = (numbers) => {
+  return numbers.reduce((total, current) => {
+    return total + current;
+  }, 0);
+};
+
+console.log(sum([1, 5, 9, 10]));
+
+const multiply = (numbers) => {
+  return numbers.reduce((total, current) => {
+    return total * current;
+  }, 1); 
+};
+
+console.log(multiply([1, 5, 9, 10]));
+
+const power = function(base, exp) {
+	return(base**exp);
+};
+
+console.log(power(2,3));
+
+const factorial = (input) => {
+  if (input === 0 || input === 1) return 1;
+  const factorialSequence = Array.from({length : input}, (v, i) => i + 1);
+  return factorialSequence.reduce((total, current) => total * current, 1); 
+};
+
+console.log(factorial(8));
 	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
-};
 
 // Do not edit below this line
 module.exports = {
