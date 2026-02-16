9248ed35ba1c099d4e101bfe55cad3232a2d0f8d
jsporle <89423014+jsporle@users.noreply.github.com>
2026-02-16 19:46:23 +0000

    pass all spec

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,6 +1,17 @@
-const fibonacci = function() {
+const fibonacci = (n) => {
+    const num = Number(n);
 
+    if (num === 0) return 0;
+    if (num === 1) return 1;
+    if (num < 0) return "OOPS";
+    let seq = [1, 1];
+    for (let i = 2; i < num; i++) {
+        seq[i] = seq[i - 1] + seq[i -2];
+    }
+    return seq[num - 1]
 };
+
+console.log(fibonacci(10))
 
 // Do not edit below this line
 module.exports = fibonacci;
