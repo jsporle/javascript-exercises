8550198a9c7e13706923d39d54071c68ae0083ad
jsporle <89423014+jsporle@users.noreply.github.com>
2026-02-18 11:22:49 +0000

    complete activities

--- foundations/15_getTheTitles/getTheTitles.js
+++ foundations/15_getTheTitles/getTheTitles.js
@@ -1,6 +1,16 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    let titles = [];
+    for (let i = 0; i < books.length; i++) {
+        titles.push(books[i].title);
+    }
+    return titles;
 };
+
+//using the map() method
+// const getTheTitles = function(books) {
+//   return books.map(book => book.title);
+// };
+
 
 // Do not edit below this line
 module.exports = getTheTitles;
--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,6 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = (people) => {
+    const currentYear = new Date().getFullYear();
+    const cleanedPeople = people.map(person => ({
+        ...person,
+        yearOfDeath: person.yearOfDeath || currentYear
+    }));
 
+    return cleanedPeople.reduce((oldest, current) => {
+        const ageOldest = oldest.yearOfDeath - oldest.yearOfBirth;
+        const ageCurrent = current.yearOfDeath - current.yearOfBirth;
+
+        return (ageCurrent > ageOldest) ? current : oldest;
+    });
 };
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
