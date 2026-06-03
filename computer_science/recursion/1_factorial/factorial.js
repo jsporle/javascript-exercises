4feae9f9d1d66e88fe4d94f9f9e2176c9e3055e9
jsporle <89423014+jsporle@users.noreply.github.com>
2026-06-03 14:15:11 +0100

    complete task 01

--- computer_science/recursion/1_factorial/factorial.js
+++ computer_science/recursion/1_factorial/factorial.js
@@ -1,6 +1,14 @@
-const factorial = function() {
-
+const factorial = function(n) {
+  if (!Number.isInteger(n) || n < 0) {
+    return undefined
+  }
+  if (n <= 1) {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
 };
+
 
 // Do not edit below this line
 module.exports = factorial;
