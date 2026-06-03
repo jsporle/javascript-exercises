de42901c58f8c22a0c555385e60d203949aaa7c7
jsporle <89423014+jsporle@users.noreply.github.com>
2026-06-03 15:20:39 +0100

    complete task 02

--- computer_science/recursion/2_contains/contains.js
+++ computer_science/recursion/2_contains/contains.js
@@ -1,5 +1,10 @@
-const contains = function() {
-  
+const contains = function(object, phrase) {
+  if (Number.isNaN(phrase) ? Number.isNaN(object) : object === phrase) return true;
+  if (typeof object === 'object' && object !== null) {
+    return Object.values(object).some(child => contains(child, phrase))
+  }
+
+  return false;
 };
   
 // Do not edit below this line
